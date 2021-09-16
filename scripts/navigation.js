0074ce9f0691ea956c6633f3221862e07313e2ba
wuyepabdul <wuyepabdul@gmail.com>
2021-09-16 21:45:53 +0100

    Add copyright to footer and replaced old files with updated files

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,10 +3,6 @@ const sections = document.querySelectorAll('section');
 const anchors = document.querySelectorAll('.navigation a');
 const nowParagraph = document.getElementById('now');
 
-const { DateTime } = luxon;
-const now = DateTime.now();
-nowParagraph.textContent = now.toLocaleString(DateTime.DATETIME_MED);
-
 navigationLinks.forEach((navlink) => {
   navlink.addEventListener('click', (e) => {
     // this makes all of the sections invisible
@@ -17,6 +13,7 @@ navigationLinks.forEach((navlink) => {
     anchors.forEach((aTag) => {
       aTag.classList.remove('activeLink');
       aTag.classList.add('inactive');
+      // aTag.style.color = 'black';
     });
 
     // make visible only the section whose link was clicked
@@ -25,5 +22,11 @@ navigationLinks.forEach((navlink) => {
     // add border only to the active navlink
     e.target.classList.remove('inactive');
     e.target.classList.add('activeLink');
+    // e.target.style.color = 'blue';
   });
 });
+
+// eslint-disable-next-line no-undef
+const { DateTime } = luxon;
+const now = DateTime.now();
+nowParagraph.textContent = now.toLocaleString(DateTime.DATETIME_MED);
