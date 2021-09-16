0c1094b9ffdbd6b5e1f8efabcd1d026b2af52bc7
wuyepabdul <wuyepabdul@gmail.com>
2021-09-16 23:02:08 +0100

    Remove comments in navigation.js and app.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,24 +5,18 @@ const nowParagraph = document.getElementById('now');
 
 navigationLinks.forEach((navlink) => {
   navlink.addEventListener('click', (e) => {
-    // this makes all of the sections invisible
     sections.forEach((section) => {
       section.classList.replace('visible', 'invisible');
     });
-    // this removes the border bottom from all of the navlinks
     anchors.forEach((aTag) => {
       aTag.classList.remove('activeLink');
       aTag.classList.add('inactive');
-      // aTag.style.color = 'black';
     });
 
-    // make visible only the section whose link was clicked
     const activeSection = document.querySelector(e.target.getAttribute('href'));
     activeSection.classList.replace('invisible', 'visible');
-    // add border only to the active navlink
     e.target.classList.remove('inactive');
     e.target.classList.add('activeLink');
-    // e.target.style.color = 'blue';
   });
 });
 
