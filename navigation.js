e701ed235f5e0b64db6cff3ec556a590d4524e38
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-16 17:01:59 +0200

    Add border bottom to active navlink

--- navigation.js
+++ navigation.js
@@ -1,5 +1,6 @@
 const navigationLinks = document.querySelectorAll('.navItem');
 const sections = document.querySelectorAll('section');
+const anchors = document.querySelectorAll('.navigation a');
 
 navigationLinks.forEach((navlink) => {
   navlink.addEventListener('click', (e) => {
@@ -7,9 +8,17 @@ navigationLinks.forEach((navlink) => {
     sections.forEach((section) => {
       section.classList.replace('visible', 'invisible');
     });
+    // this removes the border bottom from all of the navlinks
+    anchors.forEach((aTag) => {
+      aTag.classList.remove('activeLink');
+      aTag.classList.add('inactive');
+    });
 
     // make visible only the section whose link was clicked
-    const activeLink = document.querySelector(e.target.getAttribute('href'));
-    activeLink.classList.replace('invisible', 'visible');
+    const activeSection = document.querySelector(e.target.getAttribute('href'));
+    activeSection.classList.replace('invisible', 'visible');
+    // add border only to the active navlink
+    e.target.classList.remove('inactive');
+    e.target.classList.add('activeLink');
   });
 });
